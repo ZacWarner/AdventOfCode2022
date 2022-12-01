b28a483d98b53f6eefa0b4dc6bb213897ed5413b
ZacWarner <zektgn@gmail.com>
2022-12-01 08:40:20 -0800

    moved async readfile to it's own util

---
+++ utils/asyncReadFile.js
@@ -0,0 +1,18 @@
+const { promises: fsPromises } = require('fs');
+
+// ✅ read file ASYNCHRONOUSLY
+async function asyncReadFile(filename) {
+    try {
+        const contents = await fsPromises.readFile(filename, 'utf-8');
+
+        const arr = contents.split(/\r?\n/);
+
+        console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+        return arr;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default asyncReadFile(filename);
