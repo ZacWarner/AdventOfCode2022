368ab0ef3c72c865a877d7d931af564fd237bbb9
Zac Warner <zektgn@gmail.com>
2022-12-08 09:16:54 -0800

    day 7 finished

--- day7/day7.js
+++ day7/day7.js
@@ -35,7 +35,7 @@ async function part1() {
             } else if (thisDir[0] === '$') {
                 dirTree.push(thisDir[2]);
                 location++;
-            } else if (!alreadyAdded.find(file => file == thisDir[1])) {
+            } else if (!alreadyAdded.find(file => file === thisDir[1])) {
                 const check = directories.find(({ name }) => name === dirTree[location]);
                 if (check) {
                     const index = directories.findIndex(obj => {
@@ -45,13 +45,13 @@ async function part1() {
                     const newSize = directories[index].size + parseInt(thisDir[0]);
 
                     directories[index] = { ...directories[index], size: newSize };
-                    directories.forEach((dir, index) => {
-                        if (dir.name !== dirTree[location] && dir.children.length > 0) {
-                            if (dir.children.find(child => child === dirTree[location])) {
-                                directories[index].size = directories[index].size + parseInt(thisDir[0]);
-                            }
-                        }
-                    })
+                    // directories.forEach((dir, index) => {
+                    //     if (dir.name !== dirTree[location] && dir.children.length > 0) {
+                    //         if (dir.children.find(child => child === dirTree[location])) {
+                    //             directories[index].size = directories[index].size + parseInt(thisDir[0]);
+                    //         }
+                    //     }
+                    // })
                     alreadyAdded.push(thisDir[1])
                 } else {
                     const newDirectory = {
@@ -72,13 +72,13 @@ async function part1() {
                             }
                         })
                     }
-                    directories.forEach((dir, index) => {
-                        if (dir.children.length > 0) {
-                            if (dir.children.find(child => child === dirTree[location])) {
-                                directories[index].size = directories[index].size + newDirectory.size;
-                            }
-                        }
-                    })
+                    // directories.forEach((dir, index) => {
+                    //     if (dir.children.length > 0) {
+                    //         if (dir.children.find(child => child === dirTree[location])) {
+                    //             directories[index].size = directories[index].size + newDirectory.size;
+                    //         }
+                    //     }
+                    // })
 
 
                     directories.push(newDirectory);
@@ -89,7 +89,17 @@ async function part1() {
         }
     })
 
+    directories.reverse().forEach((dir, index) => {
 
+        if (dir.children.length > 0) {
+            let sum = dir.size;
+            dir.children.forEach(child => {
+                const target = directories.findIndex(({ name }) => name === child);
+                sum = sum + directories[target].size;
+            });
+            directories[index].size = sum;
+        }
+    })
 
 
     const dirAtMost = directories.filter(dir => dir.size <= 100000);
@@ -102,5 +112,5 @@ async function part1() {
 }
 
 
-//https://github.com/Unknown-Universe/AOC-2022/blob/main/Day%207/src/index.ts
+
 part1();
